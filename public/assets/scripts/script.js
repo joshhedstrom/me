84014f51271af5b8ed7faeefd4b23edc108ae0cc
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-04-25 11:10:50 -0500

    moved firebase config to js file

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
-	 $(".button-collapse").sideNav();
-	 $('.carousel').carousel();
+    $(".button-collapse").sideNav();
+    $('.carousel').carousel();
 });
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDARFh3ZZvJnPFsvZKdBvBfz58wGwoqBtY",
+    authDomain: "personalsite-55c4a.firebaseapp.com",
+    databaseURL: "https://personalsite-55c4a.firebaseio.com",
+    projectId: "personalsite-55c4a",
+    storageBucket: "personalsite-55c4a.appspot.com",
+    messagingSenderId: "659724012306"
+};
+firebase.initializeApp(config);
