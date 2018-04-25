8a2387bb5f526059e27ec62d828ba137abecce46
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-04-25 11:54:54 -0500

    added confirmation modal to contact form

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,40 +1,43 @@
 $(document).ready(function() {
     $(".button-collapse").sideNav();
     $('.carousel').carousel();
+     $('.modal').modal({
+        dismissible: false
+    });
+
+    const config = {
+        apiKey: "AIzaSyDARFh3ZZvJnPFsvZKdBvBfz58wGwoqBtY",
+        authDomain: "personalsite-55c4a.firebaseapp.com",
+        databaseURL: "https://personalsite-55c4a.firebaseio.com",
+        projectId: "personalsite-55c4a",
+        storageBucket: "personalsite-55c4a.appspot.com",
+        messagingSenderId: "659724012306"
+    };
+    firebase.initializeApp(config);
+
+    const database = firebase.database()
+
+
+    $('#customerSubmit').click((event) => {
+        event.preventDefault();
+        if ($('#customerName') !== null && $('#customerNumber') !== null && $('#customerEmail') !== null && $('#customerMessage') !== null) {
+            let name = $('#customerName').val().trim();
+            let number = $('#customerNumber').val().trim();
+            let email = $('#customerEmail').val().trim();
+            let message = $('#customerMessage').val().trim();
+
+            saveMessage(name, number, email, message);
+            $('#confirmModal').modal('open');
+        }
+    });
+
+    function saveMessage(name, number, email, message) {
+        database.ref().push({
+            name: name,
+            number: number,
+            email: email,
+            message: message,
+        })
+    }
+
 });
-
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDARFh3ZZvJnPFsvZKdBvBfz58wGwoqBtY",
-    authDomain: "personalsite-55c4a.firebaseapp.com",
-    databaseURL: "https://personalsite-55c4a.firebaseio.com",
-    projectId: "personalsite-55c4a",
-    storageBucket: "personalsite-55c4a.appspot.com",
-    messagingSenderId: "659724012306"
-};
-firebase.initializeApp(config);
-
-const database = firebase.database()
-
-
-$('#customerSubmit').click((event) => {
-	event.preventDefault();
-	if ($('#customerName') !== null && $('#customerNumber') !== null && $('#customerEmail') !== null && $('#customerMessage') !== null) {
-		let name = $('#customerName').val().trim();
-		let number = $('#customerNumber').val().trim();
-		let email = $('#customerEmail').val().trim();
-		let message = $('#customerMessage').val().trim();
-
-		saveMessage(name, number, email, message);
-	}
-});
-
-function saveMessage (name, number, email, message) {
-	database.ref().push({
-		name: name,
-		number: number,
-		email: email,
-		message: message,
-	})
-}
-
