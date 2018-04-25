c46cdf8c8912d92cf71be988ed2ca8c52e2468e4
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-04-25 11:33:47 -0500

    changed to contact form on contact.html

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -13,3 +13,28 @@ var config = {
     messagingSenderId: "659724012306"
 };
 firebase.initializeApp(config);
+
+const database = firebase.database()
+
+
+$('#customerSubmit').click((event) => {
+	event.preventDefault();
+	if ($('#customerName') !== null && $('#customerNumber') !== null && $('#customerEmail') !== null && $('#customerMessage') !== null) {
+		let name = $('#customerName').val().trim();
+		let number = $('#customerNumber').val().trim();
+		let email = $('#customerEmail').val().trim();
+		let message = $('#customerMessage').val().trim();
+
+		saveMessage(name, number, email, message);
+	}
+});
+
+function saveMessage (name, number, email, message) {
+	database.ref().push({
+		name: name,
+		number: number,
+		email: email,
+		message: message,
+	})
+}
+
