b7a7b0c34b5edea483ccbbf0180891825ee1cb5c
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-05-08 16:08:19 -0500

    fixed bug with carousel

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     $('.parallax').parallax();
-    $(".button-collapse").sideNav();
     $('.carousel').carousel();
     $('.modal').modal({
         dismissible: false
