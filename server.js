b02a1c073d6a5f5178c1f862ae2b2732ebcf3555
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-06-08 21:55:36 -0500

    npm init, added routes and server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require('morgan');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+require("./routes/html-routes.js")(app);
+
+app.listen(PORT, ()=> {
+    console.log(`App listening on http://localhost: ${PORT}`);
+})
