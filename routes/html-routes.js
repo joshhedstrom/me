ae7dc577971b3806cb30e43bc98eb94abd18b9cd
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-06-12 21:40:03 -0500

    formatted code

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,33 +3,33 @@ const nodeMailer = require('nodemailer');
 require('dotenv').config();
 
 module.exports = (app) => {
-    app.get("*", (req, res)=> {
+    app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     })
 
-    app.post('/contact', (req, res)=> {
-      let transporter = nodeMailer.createTransport({
-          host: 'smtp.gmail.com',
-          port: 465,
-          secure: true,
-          auth: {
-              user: 'jhedstrom256@gmail.com',
-              pass: process.env.GMAIL_PASSWORD
-          }
-      });
-      let mailOptions = {
-          from: req.body.email,
-          to: '"Josh Hedstrom" <josh.hedstrom@gmail.com>',
-          subject: `Contact from: ${req.body.name} || ${req.body.number}`,
-          text: `Name: ${req.body.name} \nEmail: ${req.body.email} \nPhone: ${req.body.number} \nMessage: ${req.body.message}`,
-      };
+    app.post('/contact', (req, res) => {
+        let transporter = nodeMailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'jhedstrom256@gmail.com',
+                pass: process.env.GMAIL_PASSWORD
+            }
+        });
+        let mailOptions = {
+            from: req.body.email,
+            to: '"Josh Hedstrom" <josh.hedstrom@gmail.com>',
+            subject: `Contact from: ${req.body.name} || ${req.body.number}`,
+            text: `Name: ${req.body.name} \nEmail: ${req.body.email} \nPhone: ${req.body.number} \nMessage: ${req.body.message}`,
+        };
 
-      transporter.sendMail(mailOptions, (err, info) => {
-          if (err) {
-              return console.log(err);
-          }
-          console.log('Message %s sent: %s', info.messageId, info.response);
-              res.sendFile(path.join(__dirname, "../public/index.html"));
-          });
-      });
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                return console.log(err);
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response);
+            res.sendFile(path.join(__dirname, "../public/index.html"));
+        });
+    });
 }
