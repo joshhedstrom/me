04993dc60be9d166d73056b7320b7e1910db91e7
Josh Hedstrom <josh.hedstrom@gmail.com>
2019-12-31 21:13:39 -0700

    scaffolded bio and added scroll snapping

--- client/src/pages/MainPage/MainPage.js
+++ client/src/pages/MainPage/MainPage.js
@@ -3,14 +3,16 @@ import React, { Component } from 'react';
 import HeaderComponent from '../../components/HeaderComponent/HeaderComponent';
 
 import './index.css';
+import BioComponent from '../../components/BioComponent/BioComponent';
 
 class MainPage extends Component {
   state = {};
 
   render() {
     return (
-      <div>
-        <HeaderComponent />
+      <div className="mainpage__wrapper">
+        <HeaderComponent className="scroll-child" />
+        <BioComponent className="scroll-child" />
       </div>
     );
   }
