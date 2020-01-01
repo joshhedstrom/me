b1768603ccaf8f27a2e938be13e9202b5142b5ae
Josh Hedstrom <josh.hedstrom@gmail.com>
2019-12-31 20:39:51 -0700

    scaffolded cra and main page

---
+++ client/src/components/HeaderComponent/HeaderComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.css';
+
+function HeaderComponent(props) {
+  return (
+    <div className="header__wrapper" >
+      <span className="header__title" >josh hedstrom </span>
+    </div>
+  );
+}
+
+export default HeaderComponent;
---
+++ client/src/pages/MainPage/MainPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import HeaderComponent from '../../components/HeaderComponent/HeaderComponent';
+
+import './index.css';
+
+class MainPage extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <HeaderComponent />
+      </div>
+    );
+  }
+}
+
+export default MainPage;
