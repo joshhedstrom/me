bb98f61f3894209d1b06993030d8ceda99a09363
Josh Hedstrom <josh.hedstrom@gmail.com>
2020-02-03 21:23:18 -0700

    scaffolded portfolio page

---
+++ client/src/pages/PortfolioPage/PortfolioPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './index.css';
+
+class PortfolioPage extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="">
+          
+      </div>
+    );
+  }
+}
+
+export default PortfolioPage;
